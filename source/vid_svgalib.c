7d4cbdf15876d381976bc697ca8f5ffbb68235e2
Bill Currie <bill@taniwha.org>
2001-12-22 08:21:40 +0000

    compile fix for somebody

--- source/vid_svgalib.c
+++ source/vid_svgalib.c
@@ -35,6 +35,12 @@
 # include <config.h>
 #endif
 
+#ifdef HAVE_STRING_H
+# include <string.h>
+#endif
+#ifdef HAVE_STRINGS_H
+# include <strings.h>
+#endif
 #include <stdlib.h>
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
